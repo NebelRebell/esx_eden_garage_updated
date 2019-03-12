59d7492f370bc1b66de8d2724a4a5c802967a23f
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-03-12 01:28:11 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,3 +1,9 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Eden Garage'
+
+version '1.0.0'
+
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'config.lua',
